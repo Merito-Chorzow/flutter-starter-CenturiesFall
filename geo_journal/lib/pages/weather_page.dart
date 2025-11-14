b6f9ab29df4fc36310e2ce67b3cc57854621bea8
CenturiesFall <vertez14@gmail.com>
2025-11-14 21:06:27 +0100

    API oraz strona aplikacji
    Dodanie API oraz UI

--- geo_journal/lib/pages/weather_page.dart
+++ geo_journal/lib/pages/weather_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:geo_journal/models/weather_model.dart';
+import 'package:geo_journal/services/weather_service.dart';
 
 class WeatherPage extends StatefulWidget {
   const WeatherPage({super.key});
@@ -8,8 +10,42 @@ class WeatherPage extends StatefulWidget {
 }
 
 class _WeatherPageState extends State<WeatherPage> {
+  final _weatherService = WeatherService('7679f16884822c358e3c6b7a6299a505');
+  Weather? _weather;
+
+  _fetchWeather() async {
+    String cityName = await _weatherService.getCurrentCity();
+
+    try {
+      final weather = await _weatherService.getWeather(cityName);
+      setState(() {
+        _weather = weather;
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    _fetchWeather();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(_weather?.cityName ?? "Wczytywanie miasta..."),
+
+            Text('${_weather?.temperature.round()}°C'),
+          ],
+        ),
+      ),
+    );
   }
 }
