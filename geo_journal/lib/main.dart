87a7f07565c92db6c48009b0a540d8c2665a271a
CenturiesFall <vertez14@gmail.com>
2025-11-14 23:58:47 +0100

    Aktualizacja main.dart
    Dodanie nawigacji pomiędzy miastami

--- geo_journal/lib/main.dart
+++ geo_journal/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'pages/weather_page.dart';
+import 'pages/city_weather_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,6 +14,44 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: WeatherPage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+
+  final List<Widget> _pages = [const WeatherPage(), const CityWeatherPage()];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.location_on),
+            label: 'Moja lokacja',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.location_city),
+            label: 'Wybór miasta',
+          ),
+        ],
+        onTap: (index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
+      ),
     );
   }
 }
